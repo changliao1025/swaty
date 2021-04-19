5cf50566f0f4942eef06c133fab597e698a97271
changliao1025 <changliao1025@outlook.com>
2021-04-19 14:14:16 -0700

    add aux

---
+++ README.md
@@ -0,0 +1,24 @@
+# PyEarth
+PyEarth is a Python library to support some Earth science tasks.
+
+# Content
+
+pyearth mainly provides many general purpose funcations to support other libaries.
+These functions are classified into several categories:
+1. GIS: This component provides major spatial dataset operations.
+2. Toolbox: This component provides many fuctions for data, date, math, etc.
+3. Visual: This component provides plotting function for time series, scatter, etc.
+4. System: This component provides system wide operations.
+
+
+# Acknowledgement
+This research was supported as part of the Next Generation Ecosystem Experiments-Tropics, funded by the U.S. Department of Energy, Office of Science, Office of Biological and Environmental Research at Pacific Northwest National Laboratory. The study was also partly supported by U.S. Department of Energy Office of Science Biological and Environmental Research through the Earth and Environmental System Modeling program as part of the Energy Exascale Earth System Model (E3SM) project. 
+
+# Install
+1. pip install pyearth
+
+
+# Contact
+Please contact Chang Liao (chang.liao@pnnl.gov) if you have any questions.
+
+
---
+++ setup.py
@@ -0,0 +1,68 @@
+
+import io
+import os
+import subprocess
+import shutil
+
+from setuptools import setup, find_packages, Command
+
+NAME = "pyearth"
+DESCRIPTION = \
+    "Python for Earth Science."
+AUTHOR = "Chang Liao"
+AUTHOR_EMAIL = "chang.liao@pnnl.gov"
+URL = "https://github.com/changliao1025/pyearth"
+VERSION = "0.1.1"
+REQUIRES_PYTHON = ">=3.6.0"
+KEYWORDS = "Earth Science"
+
+REQUIRED = [
+    "numpy",
+    "matplotlib",
+    "scipy",
+    "statsmodels",
+    "pandas",
+    "osgeo",
+    "netCDF4",
+    "adjustText",
+    "jdcal",
+    "pyevtk"
+]
+
+CLASSIFY = [
+    "Development Status :: 4 - Beta",
+    "Operating System :: OS Independent",
+    "Intended Audience :: Science/Research",
+    "Programming Language :: Python",
+    "Programming Language :: Python :: 3",
+    "Topic :: Scientific/Engineering",
+    "Topic :: Scientific/Engineering :: Visualization"
+]
+
+HERE = os.path.abspath(os.path.dirname(__file__))
+
+try:
+    with io.open(os.path.join(
+            HERE, "README.md"), encoding="utf-8") as f:
+        LONG_DESCRIPTION = "\n" + f.read()
+
+except FileNotFoundError:
+    LONG_DESCRIPTION = DESCRIPTION
+
+
+setup(
+    name=NAME,
+    version=VERSION,
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type="text/markdown",
+    license="custom",
+    author=AUTHOR,
+    author_email=AUTHOR_EMAIL,
+    python_requires=REQUIRES_PYTHON,
+    keywords=KEYWORDS,
+    url=URL,
+    packages=find_packages(),
+    install_requires=REQUIRED,
+    classifiers=CLASSIFY
+)
