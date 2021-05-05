d50b9281fe752e339ee7c1cb09f588d55cd1827a
changliao1025 <changliao1025@outlook.com>
2021-05-05 13:18:31 -0700

    update name

--- pyswat/shared/swat.py
+++ pyswat/shared/swat.py
@@ -1,11 +1,12 @@
 from abc import ABCMeta, abstractmethod
-
-class swat(object):
+from pyearth.system.define_global_variables import *
+class pyswat(object):
     __metaclass__ = ABCMeta
     iCase_index=0
     iSiteID=0
     iFlag_calibration=0
-
+    sWorkspace_data=''
+    sWorkspace_scratch=''
     sFilename_model_configuration=''
     sWorkspace_project=''
     sWorkspace_simulation=''
@@ -21,12 +22,16 @@ class swat(object):
     sDate_end=''
     def __init__(self, aParameter):
         self.sFilename_model_configuration    = aParameter[ 'sFilename_model_configuration']
-        self.sFilename_namelist    = aParameter[ 'sFilename_namelist']
-        #self.sWorkspace_project    = aParameter[ 'sWorkspace_project']
+
+        self.sWorkspace_data = aParameter[ 'sWorkspace_data']
+       
+        self.sWorkspace_scratch    = aParameter[ 'sWorkspace_scratch']
+        sWorkspace_scratch = self.sWorkspace_scratch
+        sWorkspace_data=self.sWorkspace_data
         self.sRegion               = aParameter[ 'sRegion']
         self.sModel                = aParameter[ 'sModel']
-        self.sDate_start              = aParameter[ 'sDate_start']
-        self.sDate_end                = aParameter[ 'sDate_end']
+        #self.sDate_start              = aParameter[ 'sDate_start']
+        #self.sDate_end                = aParameter[ 'sDate_end']
 
         self.sWorkspace_simulation = sWorkspace_scratch + slash + '04model' + slash \
             + self.sModel + slash + self.sRegion +  slash + 'simulation'
@@ -40,7 +45,7 @@ class swat(object):
 
 
         sCase_index = "{:03d}".format( int(aParameter['iCase_index']) )
-        sDate                = aParameter[ 'sDate']
+        sDate   = aParameter[ 'sDate']
         if sDate is not None:
             self.sDate= sDate
         else:
@@ -48,8 +53,7 @@ class swat(object):
             
         sCase = self.sModel + self.sDate + sCase_index
         self.sCase = sCase
-        self.iSiteID                = int(aParameter[ 'iSiteID'])
-        self.sSiteID = "{:03d}".format(self.iSiteID)
+        
 
         self.sWorkspace_simulation_case = self.sWorkspace_simulation + slash + sCase
         sPath = self.sWorkspace_simulation_case
@@ -59,13 +63,8 @@ class swat(object):
         sPath = self.sWorkspace_calibration_case
         Path(sPath).mkdir(parents=True, exist_ok=True)
 
-        self.iFlag_calibration =  int(aParameter['iFlag_calibration']) 
+        #self.iFlag_calibration =  int(aParameter['iFlag_calibration']) 
 
-        
-
-        
-        
-
-        
+                
         return
         
